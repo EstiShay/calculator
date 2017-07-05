22d590d8d27165778e9ad1dc88aae7e4cd6ca801
Esti Shay and Kate Byars <kate@>
2017-07-05 15:56:46 -0700

    add user interface.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//Business logic:
 var add = function(num1, num2) {
  return num1 + num2;
 };
@@ -14,9 +15,14 @@ var divide = function(num1, num2) {
  return num1 / num2;
 };
 
-var num1 = parseInt(prompt("Enter a number:"))
-var num2 = parseInt(prompt("Enter another number:"))
+//UI logic:
 
-
-
-alert(divide(num1, num2));
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    var num1 = parseInt($("#add1").val());
+    var num2 = parseInt($("#add2").val());
+    var result = add(num1, num2);
+    $("#output").text(result);
+  });
+});
