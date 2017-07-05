274610d09d661eeabc3d35ae77c7d505a04fdefe
Esti Shay and Kate Byars <kate@>
2017-07-05 16:19:50 -0700

    create subtract, multiply, and divide buttons.

--- js/scripts.js
+++ js/scripts.js
@@ -17,12 +17,42 @@ var divide = function(num1, num2) {
 
 //UI logic:
 
+// $(document).ready(function() {
+//   $("form#add").submit(function(event) {
+//     event.preventDefault();
+//     var num1 = parseInt($("#add1").val());
+//     var num2 = parseInt($("#add2").val());
+//     var result = add(num1, num2);
+//     $("#output").text(result);
+//   });
+
 $(document).ready(function() {
-  $("form#add").submit(function(event) {
+  $("button#add").click(function() {
     event.preventDefault();
     var num1 = parseInt($("#add1").val());
     var num2 = parseInt($("#add2").val());
-    var result = add(num1, num2);
+    var result = add(num1, num2)
+    $("#output").text(result);
+  });
+  $("button#subtract").click(function() {
+    event.preventDefault();
+    var num1 = parseInt($("#add1").val());
+    var num2 = parseInt($("#add2").val());
+    var result = subtract(num1, num2)
+    $("#output").text(result);
+  });
+  $("button#multiply").click(function() {
+    event.preventDefault();
+    var num1 = parseInt($("#add1").val());
+    var num2 = parseInt($("#add2").val());
+    var result = multiply(num1, num2)
+    $("#output").text(result);
+  });
+  $("button#divide").click(function() {
+    event.preventDefault();
+    var num1 = parseInt($("#add1").val());
+    var num2 = parseInt($("#add2").val());
+    var result = divide(num1, num2)
     $("#output").text(result);
   });
 });
